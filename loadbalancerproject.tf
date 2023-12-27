8af4044c95084c4c690bff967755be019a0708d9
Harsh-2081998 <146224313+Harsh-2081998@users.noreply.github.com>
2023-12-27 19:41:45 +0530

    Update loadbalancerproject.tf

--- loadbalancerproject.tf
+++ loadbalancerproject.tf
@@ -96,3 +96,51 @@ resource "aws_instance" "webserver2" {
         subnet_id = aws_subnet.mysubnet2.id
         user_data = base64encode(file("userdata1.sh"))
 }
+
+resource "aws_lb" "myalb" {
+	name = "myalb"
+	internal = false
+	load_balancer_type = "application"
+
+	security_groups = [aws_security_group.mysg1]
+	subnets = [aws_subnet.mysubnet1.id, aws_subnet.mysubnet2.id]
+}
+
+resource "aws_lb_target_group" "tg" {
+	name = "mytg"
+	port = 80
+	protocol = "HTTP"
+	vpc_id = aws_vpc.myvpc.id
+
+	health_check {
+		path = "/"
+		port = "traffic-port"
+}
+}
+
+resource "aws_lb_target_group_attachment" "lbattach1" {
+	target_group_arn = aws_lb_target_group.tg.arn
+	target_id = aws_instance.webserver1.id
+	port = 80
+}
+
+resource "aws_lb_target_group_attachment" "lbattach2" {
+	target_group_arn = aws_lb_target_group.tg.arn
+	target_id = aws_instance.webserver2.id
+	port = 80
+}
+
+resource "aws_lb_listener" "listener" {
+	load_balancer_arn = aws_lb_myalb.arn
+	port = 80
+	protocol = "HTTP"
+	
+	default_action {
+		target_group_arn = aws_lb_target_group.tg.arn
+		type = "forward"
+}
+}
+
+output "loadbalancerdns" {
+ value = aws_lb.myalb.dns_name
+}
